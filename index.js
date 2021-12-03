470ce4a4d6d38ae99487e84e237bcb1d1bea9fe6
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-12-03 00:01:16 -0500

    seasons updated

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const express = require('express')
 
+// const { response } = require('express')
+const express = require('express')
+const showdata = require('./showdata')
 const app = express()
 
+app.use(express.static('public'))
+
+app.set('view engine', 'pug')
+
+
 app.get('/', (request, response) => {
-  return response.send('Hello')
+  response.render('mainpage', { showdata })
 })
+
+app.get('/seasons', (request, response) => {
+  response.render('season', { title: 'Season 1, Episode 1' })
+})
+// app.get('/episode/:id', (request, response) => { // returns string
+// const episode = showdata.find((episode) => { return episode.number === parseInt(request.params.episode) })
+
+// params acess the value inside variable in '/:episode/'
+// return response.render('episodes', { episode })
+// })
 
 app.all('*', (request, response) => {
   return response.status(404).send('Sorry Bro...')
